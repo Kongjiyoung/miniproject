094d53cafecb721e22e68b9d1dc098bf461d07a5
khh <khh8500@naver.com>
2024-03-21 16:57:52 +0900

    personFindByEmail_test

--- src/test/java/com/many/miniproject1/user/UserRepositoryTest.java
+++ src/test/java/com/many/miniproject1/user/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.many.miniproject1.user;
 
 import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -9,7 +11,21 @@ import org.springframework.context.annotation.Import;
 @DataJpaTest
 public class UserRepositoryTest {
     @Autowired
-    UserRepository resumeRepository;
+    UserRepository userRepository;
     @Autowired
     private EntityManager em;
+
+    @Test
+    public void personFindByEmail_test(){
+        // given
+        String email = "captain_kong@nate.com";
+        String password = "1234";
+
+        // when
+        User user = userRepository.personFindByEmailAndPassword(email, password);
+
+        System.out.println("personFindByEmail_test: "+user);
+        // then
+        Assertions.assertThat(user.getEmail()).isEqualTo("captain_kong@nate.com");
+    }
 }
