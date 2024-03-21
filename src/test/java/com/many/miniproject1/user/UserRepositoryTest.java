b60730797a537476f10b79f04809394a54cb61d3
Kongjiyoung <ij0512@naver.com>
2024-03-21 16:28:37 +0900

    findById Test

--- src/test/java/com/many/miniproject1/user/UserRepositoryTest.java
+++ src/test/java/com/many/miniproject1/user/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.many.miniproject1.user;
 
 import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -9,7 +11,19 @@ import org.springframework.context.annotation.Import;
 @DataJpaTest
 public class UserRepositoryTest {
     @Autowired
-    UserRepository resumeRepository;
+    UserRepository userRepository;
     @Autowired
     private EntityManager em;
+
+    @Test
+    public void findById_test(){
+        // given
+        int id=14;
+
+        // when
+        User user = userRepository.findById(id);
+
+        // then
+        Assertions.assertThat(user.getCompanyName()).isEqualTo("예이");
+    }
 }
