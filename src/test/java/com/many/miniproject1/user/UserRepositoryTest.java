0dfb54460c4e1eef61a24ece9e30e4a8b5ed7c24
hyeonnit <lhyeonnit@gmail.com>
2024-03-21 16:32:14 +0900

    findById_test

--- src/test/java/com/many/miniproject1/user/UserRepositoryTest.java
+++ src/test/java/com/many/miniproject1/user/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.many.miniproject1.user;
 
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -9,7 +10,17 @@ import org.springframework.context.annotation.Import;
 @DataJpaTest
 public class UserRepositoryTest {
     @Autowired
-    UserRepository resumeRepository;
+    UserRepository userRepository;
     @Autowired
     private EntityManager em;
+
+    @Test
+        public void findById_test(){
+            // given
+        int id = 1;
+            // when
+        User user = userRepository.findById(id);
+            // then
+        System.out.println("findById_test: "+ user);
+        }
 }
